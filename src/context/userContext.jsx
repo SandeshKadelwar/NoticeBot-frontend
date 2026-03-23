e8f632128a3b6f5284c3b45b223c9d5b4b96f779
Sandesh Kadelwar <kadelwarsandesh@gmail.com>
2026-03-23 18:40:08 +0530

    userContext changed

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,13 +1,27 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useState, useEffect } from "react";
 
 export const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
+
     const [user, setUser] = useState(null);
-    return <UserContext.Provider value={{ user, setUser }}>
-        {children}
-    </UserContext.Provider>
+
+    // ✅ AUTO LOGIN FIX
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            setUser({}); // user is logged in
+        } else {
+            setUser(null);
+        }
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    )
 }
 
 export const getData = () => useContext(UserContext)
